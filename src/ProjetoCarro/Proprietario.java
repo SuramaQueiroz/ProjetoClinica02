baf9a4d5d72134af9363c327ab12e0446515e61b
26066539 <surama.queiroz90@gmail.com>
2019-03-21 20:56:11 -0300

    aula 21/03

---
+++ src/ProjetoCarro/Proprietario.java
@@ -0,0 +1,16 @@
+package ProjetoCarro;
+
+public class Proprietario {
+    String nome;
+    int rg;
+    int cpf;
+    int datadenascimento;
+    String rua;
+    String bairro;
+    String cidade;
+    String estado;
+    int cep;
+    String complemento;
+
+
+}
---
+++ src/ProjetoClinica/Paciente.java
@@ -0,0 +1,16 @@
+package ProjetoClinica;
+
+public class Paciente {
+    String nome;
+    int rg;
+    int cpf;
+    int datadenascimento;
+    String rua;
+    String bairro;
+    String cidade;
+    String estado;
+    int cep;
+    String complemento;
+    String planodesaude;
+
+}
